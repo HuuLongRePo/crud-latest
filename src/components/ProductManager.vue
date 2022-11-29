d78101c8d60931a98c193df61c2b6cbd40c9e2c6
HuuLongRePo <nhuulong2021@gmail.com>
2022-11-29 19:34:56 +0700

    toi 29-11

---
+++ src/components/ProductManager.vue
@@ -0,0 +1,50 @@
+<template>
+    <div>
+        {{ products }}
+        <DataTable :value="products" responsiveLayout="scroll">
+            <DataColumn field="id" header="Code"></DataColumn>
+            <DataColumn field="name" header="Name"></DataColumn>
+            <DataColumn field="year" header="Category"></DataColumn>
+            <DataColumn field="color" header="Quantity"></DataColumn>
+        </DataTable>
+    </div>
+</template>
+
+<script>
+// import { $fetch } from 'ohmyfetch'
+// export default {
+//     setup() {},
+//     data() {
+//         return {
+//             products : []
+//         }
+//     },
+//     mounted() {
+//         this.loadProducts()
+//     },
+//     methods: {
+//         async loadProducts() {
+//             const res = await $fetch('https://reqres.in/api/user')
+//             if(res && res.data) {
+//                 this.products = res.data
+//             }
+//         }
+//     }
+// }  
+export default{
+    data() {
+    return {
+        products:[]
+    }
+},
+mounted() {
+    this.fetchProduct();
+},
+methods: {
+     fetchProduct(){
+        return  fetch('https://reqres.in/api/user').then(res=>res.json()).then(data=>this.products=data.data);
+    }
+},
+}
+
+</script>
--- src/main.js
+++ src/main.js
@@ -1,4 +1,17 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import DataTable from 'primevue/datatable';
+import Column from 'primevue/column';
 
-createApp(App).mount('#app')
+import PrimeVue from 'primevue/config';
+import Dialog from 'primevue/dialog';
+const app = createApp(App);
+
+app.use(PrimeVue);
+
+
+app.component('DataTable', DataTable);
+app.component('DataColumn', Column);
+app.component('ProductDialog', Dialog);
+
+app.mount('#app')
